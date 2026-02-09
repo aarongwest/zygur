2e23425c19f2e479b232060a96921e4ce32fada8
Aaron West <aaron@oshify.com>
2026-02-08 22:42:20 -0700

    Make main background transparent to show mint body background

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-white dark:bg-black text-black dark:text-white">
+    <main className="min-h-screen bg-transparent text-brand-grey">
       <Navigation />
       <Hero />
       <Features />
